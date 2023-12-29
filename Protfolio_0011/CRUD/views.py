1498b70e10a90ca70c286d50c27980c6ae6c108f
Monayem82 <mamonayem6@gmail.com>
2023-12-29 15:55:28 +0600

    Login done

--- Protfolio_0011/CRUD/views.py
+++ Protfolio_0011/CRUD/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from CRUD.forms import CreateUser,loginUser
+from django.contrib.auth import login,logout,authenticate
 
 
 # Create your views here.
@@ -17,5 +19,15 @@ def createUserViews(request):
 #     return render(request,'CRUD/showUser.html',context={"users":users})
 
 def loginUserView(request):
-    user=loginUser()
-    return render(request,'CRUD/loginUser.html',context={'user':user})
+    if request.method=='POST':
+        frm=loginUser(request=request,data=request.POST)
+        if frm.is_valid():
+            usern=frm.cleaned_data['username']
+            passn=frm.cleaned_data['password']
+            user=authenticate(username=usern,password=passn)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect('/crud/createUsers/')
+    else:
+        frm=loginUser()
+    return render(request,'CRUD/loginUser.html',context={'user':frm})
